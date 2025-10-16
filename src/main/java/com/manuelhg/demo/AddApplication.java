98cb7ff1cd3c7141ec82a46bde7a1f8f5536146c
Manuel HG <mhergar093#g.educaand.es>
2025-10-16 12:51:26 +0200

    Plantilla base

---
+++ src/main/java/com/manuelhg/demo/AddApplication.java
@@ -0,0 +1,20 @@
+package com.manuelhg.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class AddApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("hola");
+    }
+}
